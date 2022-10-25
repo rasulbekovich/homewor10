32b537eedf4bbc99b4d3ed2c7ffd4cf6ad39835d
Yntymak <yzenisbekov7@gmail.com>
2022-10-25 12:12:06 +0600

    homework 10 with code

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Random;
+import java.util.Scanner;
+
+            public class Main {
+            public static void main(String[] args) {
+            contains(5,1,2,3,4,5 );}
+            public static boolean contains(int a, int... varargs) {
+            for (int vararg : varargs) {
+            System.out.print(vararg+" ");
+            System.out.println(vararg == a);}
+            return false;
+            }
+            }
